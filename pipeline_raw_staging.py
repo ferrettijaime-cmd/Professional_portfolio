8139704be7a1de7d5837f06f36bfabb9618d68f8
Jaime <ferretti.jaime@gmail.com>
2026-05-19 18:52:33 +0200

    Update script

--- pipeline_raw_staging.py
+++ pipeline_raw_staging.py
@@ -2,13 +2,6 @@ import os
 import psycopg2
 from dotenv import load_dotenv
 
-folder = r"C:\Professional_project"
-file = "staging_script.sql"
-path = os.path.join(folder, file)
-
-with open(path, "r", encoding="utf-8") as f:
-    sql = f.read()
-
 load_dotenv()
 user = os.getenv("DB_USER")
 password = os.getenv("DB_PASSWORD")
@@ -16,14 +9,34 @@ host = os.getenv("DB_HOST")
 port = os.getenv("DB_PORT")
 database = os.getenv("DB_NAME")
 
-connection = psycopg2.connect(host=host, port=port, database=database, user=user, password=password)
+cursor = None
+connection = None
 
-cursor = connection.cursor()
-cursor.execute(sql)
-connection.commit()
+try:
+    folder = r"C:\Professional_project"
+    file = "staging_script.sql"
+    path = os.path.join(folder, file)
 
-cursor.close()
-connection.close()
-print("Pipeline successfully constructed")
+    with open(path, "r", encoding="utf-8") as f:
+        sql = f.read()
 
+    connection = psycopg2.connect(host=host, port=port, database=database, user=user, password=password)
+
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    connection.commit()
+    print("Pipeline successfully constructed")
+
+except FileNotFoundError:
+    print(f"The file was not found in the path:{path}")
+
+except Exception as e:
+    if connection:
+        connection.rollback()
+    print(f'Error details:{e}')
+finally:
+    if cursor:
+        cursor.close()
+    if connection:
+        connection.close()
 
