730b5256d07f74bb12fc5044f88d37f017928e22
Jaime <ferretti.jaime@gmail.com>
2026-05-28 11:38:26 +0200

    update script

--- Olist-Logistics-Analysis/Scripts Python/extract_data.py
+++ Olist-Logistics-Analysis/Scripts Python/extract_data.py
@@ -20,63 +20,72 @@ host = os.getenv("DB_HOST")
 port = os.getenv("DB_PORT")
 database = os.getenv("DB_NAME")
 
-engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")
+try:
+    engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")
 
-customer_df.to_sql(name="olist_customers_dataset",
+    customer_df.to_sql(name="olist_customers_dataset",
                    con=engine,
                    schema="raw",
                    if_exists="replace",
                    index=False)
 
-order_items_df.to_sql(name="olist_orders_items_dataset",
+    order_items_df.to_sql(name="olist_orders_items_dataset",
                    con=engine,
                    schema="raw",
                    if_exists="replace",
                    index=False)
 
-order_payments_df.to_sql(name="olist_orders_payments_dataset",
+    order_payments_df.to_sql(name="olist_orders_payments_dataset",
                    con=engine,
                    schema="raw",
                    if_exists="replace",
                    index=False)
 
-order_reviews_df.to_sql(name="olist_orders_reviews_dataset",
+    order_reviews_df.to_sql(name="olist_orders_reviews_dataset",
                    con=engine,
                    schema="raw",
                    if_exists="replace",
                    index=False)
 
-orders_df.to_sql(name="olist_orders_dataset",
+    orders_df.to_sql(name="olist_orders_dataset",
                    con=engine,
                    schema="raw",
                    if_exists="replace",
                    index=False)
 
-products_df.to_sql(name="olist_products_dataset",
+    products_df.to_sql(name="olist_products_dataset",
                    con=engine,
                    schema="raw",
                    if_exists="replace",
                    index=False)
 
-geolocation_df.to_sql(name="olist_geolocation_dataset",
+    geolocation_df.to_sql(name="olist_geolocation_dataset",
                    con=engine,
                    schema="raw",
                    if_exists="replace",
                    index=False)
 
-sellers_df.to_sql(name="olist_sellers_dataset",
+    sellers_df.to_sql(name="olist_sellers_dataset",
                    con=engine,
                    schema="raw",
                    if_exists="replace",
                    index=False)
 
-product_category_name_translation_df.to_sql(name="product_category_name_translation_df",
+    product_category_name_translation_df.to_sql(name="product_category_name_translation_df",
                    con=engine,
                    schema="raw",
                    if_exists="replace",
                    index=False)
+    
+except Exception as e:
+    print(f'Detail: {e}')
+    
+finally:
+    engine.dispose()
+       
+       
 
-print("The data was sent successfully!!!")
+
 
 
 
