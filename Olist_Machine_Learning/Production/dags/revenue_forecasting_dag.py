a5a42c1b11889a10310b5310798008555e1981b1
Jaime <ferretti.jaime@gmail.com>
2026-06-22 18:08:57 +0200

    pipeline to extract the revenue dataset

--- Olist_Machine_Learning/Production/dags/revenue_forecasting_dag.py
+++ Olist_Machine_Learning/Production/dags/revenue_forecasting_dag.py
@@ -1,23 +1,53 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+from sqlalchemy.engine import URL
 from sqlalchemy import create_engine
 import pandas as pd
 
-base_dir = Path(__file__).resolve().parent.parent
-file = base_dir / ".neondb_credentials.env"
+base_dir_neon = Path(__file__).resolve().parent.parent.parent
+file_neondb = base_dir_neon / "neondb_credentials.env"
 
-load_dotenv(file)
-user = os.getenv("DB_USER")
-password = os.getenv("DB_PASSWORD")
-host = os.getenv("DB_HOST")
-port = os.getenv("DB_PORT")
-database = os.getenv("DB_NAME")
+base_dir_local = Path(__file__).resolve().parent.parent.parent
+file_ml_foresc = base_dir_local / "ml_forecasting_db.env"
 
-engine = None
+load_dotenv(file_neondb)
+neon_user = os.getenv("DB_USER")
+neon_password = os.getenv("DB_PASSWORD")
+neon_host = os.getenv("DB_HOST")
+neon_port = os.getenv("DB_PORT")
+neon_database = os.getenv("DB_NAME")
+
+load_dotenv(file_ml_foresc, override=True)
+local_user = os.getenv("POSTGRES_USER")
+local_password = os.getenv("POSTGRES_PASSWORD")
+local_host = os.getenv("POSTGRES_HOST")
+local_port = os.getenv("POSTGRES_PORT")
+local_database = os.getenv("POSTGRES_DB")
+
+neon_url = URL.create(
+    drivername="postgresql+psycopg2",
+    username=neon_user,
+    password=neon_password,
+    host=neon_host,
+    port=neon_port,
+    database=neon_database
+)
+
+local_url = URL.create(
+    drivername="postgresql+psycopg2",
+    username=local_user,
+    password=local_password,
+    host=local_host,
+    port=local_port,
+    database=local_database
+)
+
+engine_neon = None
+engine_local = None
 
 try:
-    engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")
+    engine_neon = create_engine(neon_url)
     query="""
         select
             order_purchase_timestamp::date as order_day,
@@ -31,14 +61,23 @@ try:
         order by 
         order_day
     """
+    revenue_df = pd.read_sql(query, con=engine_neon)
+    print(f"Rows extracted: {len(revenue_df)}")
 
-    revenue_df = pd.read_sql(query, con=engine)
-    revenue_df.to_csv("revenue_dataset.csv", index=False)
+    engine_local = create_engine(local_url)
+
+    revenue_df.to_sql(name="revenue_dataset",
+                   con=engine_local,
+                   schema="raw",
+                   if_exists="replace",
+                   index=False)
     print("The dataset was successfully loaded!!!")
-
+    
 except Exception as e:
-    print(f"Detail error: {e}")
+    import traceback
+    traceback.print_exc()
 finally:
-    if engine:
-        engine.dispose()
-        
+    if engine_neon:
+        engine_neon.dispose()
+    if engine_local:
+        engine_local.dispose()
