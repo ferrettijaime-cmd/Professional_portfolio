c8d435bf4764a1a06ae39739f9e8584148a43e01
Jaime <ferretti.jaime@gmail.com>
2026-07-05 18:11:52 +0200

    updated script

--- MongoDB_DataLake/script_pyton/upload_to_bronze.py
+++ MongoDB_DataLake/script_pyton/upload_to_bronze.py
@@ -1,14 +1,23 @@
-from dotenv import load_dotenv
 import os
-from azure.storage.filedatalake import DataLakeServiceClient
-from pymongo import MongoClient
 import json
+from datetime import datetime
 
-load_dotenv("MongoDB_DataLake/azure_credential.env")
+from dotenv import load_dotenv
+from pymongo import MongoClient
+from azure.storage.filedatalake import DataLakeServiceClient
+
+load_dotenv("MongoDB_DataLake/credentials.env")
+MONGO_USER = os.getenv("MONGO_USER")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+MONGO_CLUSTER = os.getenv("MONGO_CLUSTER")
 ACCOUNT_NAME = os.getenv("AZURE_STORAGE_ACCOUNT")
 ACCOUNT_KEY = os.getenv("AZURE_STORAGE_KEY")
 
 try:
+    uri = f'mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_CLUSTER}/'
+    client = MongoClient(uri)
+    print("Connected successfully to MongoDB Atlas!")
+
     ACCOUNT_URL = f"https://{ACCOUNT_NAME}.dfs.core.windows.net"
 
     service_client = DataLakeServiceClient(
@@ -16,52 +25,50 @@ try:
     credential=ACCOUNT_KEY
     )
 
-    print("Connected successfully!")
-
-    file_system_client = service_client.get_file_system_client(
-    file_system="bronze"
-    )
+    print("Connected successfully to Azure!")
 
 except Exception as e:
-    print(f"Azure error detail: {e}")
-
-load_dotenv("MongoDB_DataLake/credentials.env")
-MONGO_USER = os.getenv("MONGO_USER")
-MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
-MONGO_CLUSTER = os.getenv("MONGO_CLUSTER")
+    print(f"Connection error: {e}")
 
 try:
-    uri = f'mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_CLUSTER}/'
-    client = MongoClient(uri)
-    print("Connected successfully to MongoDB Atlas!")
-
     db = client["bank_db"]
     transactions = db["transactions"]
 
     documents = list(transactions.find({}, {"_id": 0}))
+    print(f"{len(documents)} documents extracted.")
 
-    json_data = json.dumps(
-    documents,
-    indent=4,
-    default=str
-    )
+    file_system_client = service_client.get_file_system_client("bronze")
 
     directory_client = file_system_client.get_directory_client("mongodb")
 
-    directory_client.create_directory()
+    try:
+        directory_client.create_directory()
+    except:
+        pass
 
-    file_client = directory_client.create_file("transactions.json")
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-    file_client.append_data(
-    data=json_data,
-    offset=0,
-    length=len(json_data)
+    filename = f"transactions_{timestamp}.json"
+
+    file_client = directory_client.create_file(filename)
+
+    ndjson = "\n".join(
+        json.dumps(doc, default=str)
+        for doc in documents
     )
 
-    file_client.flush_data(len(json_data))
+    file_client.append_data(
+        data=ndjson,
+        offset=0,
+        length=len(ndjson)
+    )
+
+    file_client.flush_data(len(ndjson))
+
+    print(f"File '{filename}' uploaded successfully!")
 
 except Exception as e:
-    print(f"MongoDB error detail: {e}")
+    print(e)
 
 
 
